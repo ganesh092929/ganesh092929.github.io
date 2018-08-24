f06f0f5142b0773e399b5c3d78aeb531964f8df1
ganesh092929 <ganesh092929@gmail.com>
2018-08-24 23:18:44 +0800

    List of Video Data + Percentage + Slider

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
-const radioBtn_ChID = document.getElementById('radioBtn-ChID');
-const radioBtn_ChUsername = document.getElementById('radioBtn-ChUsername');
+const sliderInput = document.getElementById('slider-input');
 const channelData = document.getElementById('channel-data');
 const videoContainer = document.getElementById('video-container');
 
-var ChID,ChUsername;
+var ChID, ChUsername,CopyData, inputURL, inputPercentage;
 var APIkey = 'AIzaSyBt7BBMDq9yrrhwBDN3exkk0t9hY0waK9A';
-var CopyData;
+
 channelForm.addEventListener('submit', e => {
+  ChID = null;
   e.preventDefault();
-  if (radioBtn_ChID.checked) {
-    ChID = channelInput.value;
+  inputURL = channelInput.value;
+  inputPercentage = sliderInput.value;
+  channelData.innerHTML = null;
+
+  if (inputURL.indexOf("channel")>0) {
+    ChID = inputURL.substring(inputURL.indexOf("channel")+8,inputURL.length);
+    console.log(ChID);
     getChannelData();
     channelData.style.display = "block";
     videoContainer.style.display = "block";
@@ -24,8 +29,8 @@ channelForm.addEventListener('submit', e => {
         part: 'snippet,statistics,brandingSettings'
     }
     $.getJSON(URL_ChSearch,options_ChSearch,getChannelData);
-  } else {
-    ChUsername = channelInput.value;
+  } else if (inputURL.indexOf("user")>0) {
+    ChUsername = inputURL.substring(inputURL.indexOf("user")+5,inputURL.length);
     getChannelData();
     channelData.style.display = "block";
     videoContainer.style.display = "block";
@@ -47,6 +52,7 @@ channelForm.addEventListener('submit', e => {
 var videoCnt,videoData;
 function ShowChannelData(){
     if (CopyData) {
+      ChID = CopyData.items[0].id;
       videoCnt = CopyData.items[0].statistics.videoCount;
       const output = `
         <img src=${CopyData.items[0].brandingSettings.image.bannerImageUrl} width=100%>
@@ -62,68 +68,61 @@ function ShowChannelData(){
       const channelData = document.getElementById('channel-data');
       channelData.innerHTML = output;
 
-/*      videoData = new Array(videoCnt-1);
-      for (var i = 0; i < videoCnt; i++) {
-        videoData[i] = new Array(2);
-      }
-      API_videoID();*/
+      API_videoID();
     }
 }
-var URL_VideoSearch,options_VideoSearch,nxtPgToken;
-function API_videoID(callback){
-  for (var k = 1; k <= Math.ceil(videoCnt/50); k++){
+
+var totalVideoCnt, URL_VideoSearch,options_VideoSearch,nxtPgToken;
+function API_videoID(nxtPgToken){
     URL_VideoSearch = 'https://www.googleapis.com/youtube/v3/search';
     options_VideoSearch = {
             part: 'snippet',
             channelId: ChID,
             maxResults: 50,
             key: APIkey,
-            nextPageToken: nxtPgToken
+            pageToken: nxtPgToken
     }
-    console.log(options_VideoSearch);
     $.getJSON(URL_VideoSearch,options_VideoSearch,getVideoData)
-  }
-  //API_videoStats();
 
-  function getVideoData(data,nextPageToken){
-      CopyData = data;
-      if (CopyData) {
-         StoreVideoID();
-         CopyData.nextPageToken = nextPageToken;
-      }
+  function getVideoData(data){
+    CopyData = data;
+    totalVideoCnt = CopyData.pageInfo.totalResults;
+    StoreVideoID();
+    if (currCnt < totalVideoCnt) { 
+      API_videoID(CopyData.nextPageToken);
+    }
   }
 }
+var indvCnt = 0;
 var currCnt = 0;
 function StoreVideoID(){
-  for (var j = 0; j < CopyData.itEms.length; j++){
-      videoData[currCnt][0] = CopyData.items[j].id.videoId;
+  for (var j = 0; j <= CopyData.items.length-1; j++){
+      //console.log (currCnt + ") " + CopyData.items[indvCnt].id.videoId);
+      API_videoStats(currCnt, CopyData.items[indvCnt].id.videoId);
       currCnt += 1
-      if (currCnt=> videoCnt){break;}
+      indvCnt += 1
+      if (indvCnt == 50){indvCnt = 0;}
   }
-  nxtPgToken = CopyData.nextPageToken;
 }
 
-function API_videoStats(){
-  for (var k = 0; k < videoCnt; k++){
-    console.log(videoData[k][0]);
-    if (videoData[k][0]){
-      var URL_VideoDetails = 'https://www.googleapis.com/youtube/v3/videos';
-      var options_VideoDetails = {
-          part: 'snippet,contentDetails,statistics',
-          id: videoData[k][0],
-          key: APIkey
-      }
-      $.getJSON(URL_VideoDetails,options_VideoDetails,getVideoStats)
-      function getVideoStats(data){
-        CopyData = data;
-        //StoreVideoStats();
-      }
-    }
+var videoTitle, videoLikes, videoViews;
+function API_videoStats(currCnt, videoID){
+  var URL_VideoDetails = 'https://www.googleapis.com/youtube/v3/videos';
+  var options_VideoDetails = {
+      part: 'snippet,contentDetails,statistics',
+      id: videoID,
+      key: APIkey
   }
-}
-function StoreVideoStats(){
-  for (var j = 0; j < videoData.length; j++){
-    videoData[j][1] = CopyData.items[0].id.videoId;
+  $.getJSON(URL_VideoDetails,options_VideoDetails,getVideoStats)
+  function getVideoStats(data){
+    CopyData = data;
+    //Console.log(CopyData);
+    videoTitle = CopyData.items[0].snippet.title;
+    videoLikes = CopyData.items[0].statistics.likeCount;
+    videoViews = CopyData.items[0].statistics.viewCount;
+    PercentLikes = (videoLikes/videoViews*100).toFixed(2);
+    output = `<p><a href="https:\\/\\/www.youtube.com/watch?v=${videoID}" target="_blank">${videoTitle}</a> ${PercentLikes}%</p>`;
+    if (parseFloat(PercentLikes) > parseFloat(inputPercentage)) { channelData.innerHTML += output; }
   }
 }
 
